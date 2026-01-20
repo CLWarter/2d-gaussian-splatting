d6de6b9cd8964de50ea1c0e3c0d0f8879fc410fc
CLWarter <cw.grafiko@gmail.com>
2026-01-20 11:47:44 +0100

    learnable ambient

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -55,6 +55,7 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
     means3D = pc.get_xyz
     means2D = screenspace_points
     opacity = pc.get_opacity
+    ambient = pc.get_ambient
 
     # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
     # scaling / rotation by the rasterizer.
@@ -100,6 +101,7 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         shs = shs,
         colors_precomp = colors_precomp,
         opacities = opacity,
+        ambients = ambient,
         scales = scales,
         rotations = rotations,
         cov3D_precomp = cov3D_precomp
